71f6aa48c414baf920af44a56e4a051823b77645
gengdian <1051166333@qq.com>
2022-07-05 14:27:19 +0800

    bug fix

--- testcase/test_report.py
+++ testcase/test_report.py
@@ -1,13 +1,21 @@
+import os
+import sys
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(basedir)[0]
+sys.path.append(rootPath)
+
 import time  # 导入时间模块 方便创建文件时创建不同文件
 import unittest
 import HTMLTestRunner  # 生成HTML文件模块
 from base.SendEmail import Send_Email
 
-dir = "D:\\py_project\\chicv\\testcase"  # 获取当前文件路径
 
-suite = unittest.defaultTestLoader.discover(start_dir=dir, pattern="test_case.py")  # 获取要运行的测试用例文件
 
-if __name__ == '__main__':
+def zhix():
+    dir = "D:\\py_project\\chicv\\testcase"  # 获取当前文件路径
+    print(dir)
+    suite = unittest.defaultTestLoader.discover(start_dir=dir, pattern="test_case.py")  # 获取要运行的测试用例文件
     report_dir = "D:\\py_project\\chicv\\test_report"  # 获取存放测试用例文件路径
     now = time.strftime("%Y-%m-%d %H_%M_%S")  # 获取当前时间
     report_name = report_dir + '/' + now + "result.html"  # 拼接时间跟result生成html文件，并放在report_dir目录下
@@ -21,3 +29,5 @@ if __name__ == '__main__':
     Send_Email().send_emails()
 
 
+if __name__ == '__main__':
+    zhix()
